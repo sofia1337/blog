b5d3de9c39e89cdacf5a466bd6d37481bb412e3b
sofia1337 <sofiia.stepankiv@oa.edu.ua>
2021-03-30 11:43:56 +0300

    Labor #4

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
-use App\Http\Controllers\Controller;
+//use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+use Illuminate\Support\Str;
 
-class CategoryController extends Controller
+class CategoryController extends BaseController
 {
     /**
      * Display a listing of the resource.
@@ -15,6 +17,11 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        //dd(__METHOD__);
+        $paginator = BlogCategory::paginate(5);
+
+        return view('blog.admin.categories.index', compact('paginator'));
+
     }
 
     /**
@@ -25,6 +32,7 @@ class CategoryController extends Controller
     public function create()
     {
         //
+        //dd(__METHOD__);
     }
 
     /**
@@ -36,6 +44,7 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         //
+        //dd(__METHOD__);
     }
 
     /**
@@ -47,6 +56,7 @@ class CategoryController extends Controller
     public function show($id)
     {
         //
+        //dd(__METHOD__);
     }
 
     /**
@@ -58,6 +68,11 @@ class CategoryController extends Controller
     public function edit($id)
     {
         //
+        //dd(__METHOD__);
+        $item = BlogCategory::findOrFail($id);
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
     /**
@@ -70,7 +85,32 @@ class CategoryController extends Controller
     public function update(Request $request, $id)
     {
         //
+        //dd(__METHOD__);
+        $item = BlogCategory::find($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return back() //redirect back
+            ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"]) //видати помилку
+            ->withInput(); //повернути дані
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+
+        $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.categories.edit', $item->id)
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->with(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
+
 
     /**
      * Remove the specified resource from storage.
@@ -81,5 +121,7 @@ class CategoryController extends Controller
     public function destroy($id)
     {
         //
+        //dd(__METHOD__);
+
     }
 }
